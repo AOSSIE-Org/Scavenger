38e015e5e5f1ea7b2132a5a270a3ce3f01c88946
itegulov <ditegulov@gmail.com>
2017-03-04 13:14:17 +0300

    Change Model's comment to actual documentation

--- src/main/scala/au/aossie/scavenger/model/Model.scala
+++ src/main/scala/au/aossie/scavenger/model/Model.scala
@@ -3,9 +3,11 @@ package au.aossie.scavenger.model
 import au.aossie.scavenger.expression.E
 
 abstract class Model {
-
-  // returns 'Some(true)' if 'e' is true in the model,
-  //         'Some(false)' if 'e' is false in the model,
-  //         'None' if the truth value of 'e' is unknown in the model.
+  /**
+    * @param e expression to check
+    * @return `Some(true)` if `e` is true in the model,
+    *         `Some(false)` if `e` is false in the model,
+    *         `None` if the truth value of `e` is unknown in the model.
+    */
   def truthValue(e: E): Option[Boolean]
 }
